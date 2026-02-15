3bd12a502978263f0f962302a59f3c443992db0d
Daniel Lewin-Ray <dlewinray@gmail.com>
2026-02-15 16:24:11 +0000

    Updated tsekI.c to run tsekW_ functions

--- src/tsekI.c
+++ src/tsekI.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include "tsekI.h"
 #include "linux/tsekL.h"
+#include "windows/tsekW.h"
 
 void tsekI_init(tsekIContext* context, tsekIWindow *window, tsekIWindowInfo *info, bool createGlobalContext, bool console) {
 #ifdef PLATFORM_LINUX
@@ -17,96 +18,128 @@ void tsekI_init(tsekIContext* context, tsekIWindow *window, tsekIWindowInfo *inf
 void tsekI_fill_context(tsekIContext* context, bool setGlobal) {
 #ifdef PLATFORM_LINUX
   tsekL_fill_context(context, setGlobal);
+#elif defined PLATFORM_WINDOWS
+  tsekW_fill_context(context, setGlobal);
 #endif
 }
 
 void tsekI_destroy_context(tsekIContext *context) {
 #ifdef PLATFORM_LINUX
   tsekL_destroy_context(context);
+#elif defined PLATFORM_WINDOWS
+  tsekW_destroy_context(context);
 #endif
 }
 
 void tsekI_create_dummy_window(tsekIWindow* window) {
 #ifdef PLATFORM_LINUX
   tsekL_create_dummy_window(window);
+#elif defined PLATFORM_WINDOWS
+  tsekW_create_dummy_window(window);
 #endif
 }
 
 void tsekI_create_window(tsekIWindow* window, tsekIWindowInfo *info) {
 #ifdef PLATFORM_LINUX
   tsekL_create_window(window, info);
+#elif defined PLATFORM_WINDOWS
+  tsekW_create_window(window, info);
 #endif
 }
 
 void tsekI_destroy_window(tsekIWindow *window) {
 #ifdef PLATFORM_LINUX
   tsekL_destroy_window(window);
+#elif defined PLATFORM_WINDOWS
+  tsekW_destroy_window(window);
 #endif
 }
 
 bool tsekI_get_closed_window(tsekIWindow *window) {
 #ifdef PLATFORM_LINUX
   return tsekL_get_closed_window(window);
+#elif defined PLATFORM_WINDOWS
+  return tsekW_get_closed_window(window);
 #endif
 }
 
 bool tsekI_update_window(tsekIWindow *window) {
 #ifdef PLATFORM_LINUX
   return tsekL_update_window(window);
+#elif defined PLATFORM_WINDOWS
+  return tsekW_update_window(window);
 #endif
 }
 
 double tsekI_get_time() {
 #ifdef PLATFORM_LINUX
   return tsekL_get_time();
+#elif defined PLATFORM_WINDOWS
+  return tsekW_get_time();
 #endif
 }
 
 double tsekI_get_fixed_time() {
 #ifdef PLATFORM_LINUX
   return tsekL_get_fixed_time();
+#elif defined PLATFORM_WINDOWS
+  return tsekW_get_fixed_time();
 #endif
 }
 
 void tsekI_set_time(double time) {
 #ifdef PLATFORM_LINUX
   tsekL_set_time(time);
+#elif defined PLATFORM_WINDOWS
+  tsekW_set_time(time);
 #endif
 }
 
 void tsekI_allocate_time(double framerate, double start, double end) {
 #ifdef PLATFORM_LINUX
   tsekL_allocate_time(framerate, start, end);
+#elif defined PLATFORM_WINDOWS
+  tsekW_allocate_time(framerate, start, end);
 #endif
 }
 
 bool tsekI_get_cursor_visible(tsekIWindow* window) {
 #ifdef PLATFORM_LINUX
   return tsekL_get_cursor_visible(window);
+#elif defined PLATFORM_WINDOWS
+  return tsekW_get_cursor_visible(window);
 #endif
 }
 
 void tsekI_set_cursor_visible(tsekIWindow* window, bool active) {
 #ifdef PLATFORM_LINUX
   tsekL_set_cursor_visible(window, active);
+#elif defined PLATFORM_WINDOWS
+  tsekW_set_cursor_visible(window, active);
 #endif
 }
 
 void tsekI_swap_buffers(tsekIWindow* window) {
 #ifdef PLATFORM_LINUX
   tsekL_swap_buffers(window);
+#elif defined PLATFORM_WINDOWS
+  tsekW_swap_buffers(window);
 #endif
 }
 
 void tsekI_get_window_param(tsekIWindow *window, tsekIWindowParam param, void *out) {
 #ifdef PLATFORM_LINUX
   tsekL_get_window_param(window, param, out);
+#elif defined PLATFORM_WINDOWS
+  tsekW_get_window_param(window, param, out);
 #endif
 }
 
 void tsekI_set_window_param(tsekIWindow *window, tsekIWindowParam param, void *in) {
 #ifdef PLATFORM_LINUX
   tsekL_set_window_param(window, param, in);
+#elif defined PLATFORM_WINDOWS
+  tsekW_set_window_param(window, param, in);
 #endif
 }
 
---
+++ src/windows/tsekW.h
@@ -0,0 +1,31 @@
+#ifdef PLATFORM_WINDOWS
+
+void tsekW_init(tsekIContext*, tsekIWindow*, tsekIWindowInfo*, bool createGlobalContext, bool console);
+
+void tsekW_fill_context(tsekIContext* context, bool setGlobal);
+void tsekW_destroy_context(tsekIContext* context);
+
+void tsekW_create_dummy_window(tsekIWindow* window);
+void tsekW_create_window(tsekIWindow* window, tsekIWindowInfo* info);
+void tsekW_destroy_window(tsekIWindow* window);
+
+bool tsekW_get_closed_window(tsekIWindow*);
+bool tsekW_update_window(tsekIWindow* window);
+
+double tsekW_get_time();
+double tsekW_get_fixed_time();
+
+void tsekW_set_time(double time);
+void tsekW_allocate_time(double framerate, double start, double end);
+
+bool tsekW_get_cursor_visible(tsekIWindow*);
+void tsekW_set_cursor_visible(tsekIWindow*, bool);
+
+void tsekW_swap_buffers(tsekIWindow*);
+
+// messager
+
+void tsekW_get_window_param(tsekIWindow* window, tsekIWindowParam param, void* out);
+void tsekW_set_window_param(tsekIWindow* window, tsekIWindowParam param, void* in);
+
+#endif
